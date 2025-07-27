b686afaa8a1145e138df88cb7c26ab9b867e6c9f
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-27 18:45:32 +0100

    file system

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
  const http = require('http');
+const fs = require('fs');
+
 
  const server = http.createServer((req, res) =>{
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write("<html>");
         res.write("<head><title>Enter Message</title><head>");
@@ -9,6 +12,12 @@
         res.write("</html>");
         return res.end();
     }
+if (url === '/message' && method === 'POST') {
+  fs.writeFileSync('message.txt', 'DUMMY');
+  res.statusCode = 302;
+  res.setHeader('location', '/');
+  return res.end();
+}
   res.setHeader('Content-Type', 'text/html')
    res.write('<html>');
    res.write('<head><title>My First Page</title><head>');
