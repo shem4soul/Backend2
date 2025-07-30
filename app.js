fd23722a8d8dd0b473dc2530b368203a90986ad5
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-30 18:57:09 +0100

    fix: update server port to 3001 and add console log for server status

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ const routes = require('./routes')
 
  const server = http.createServer(routes)
 
-     server.listen(3000);
+     server.listen(3001);
+     console.log("Server is running on http://localhost:3001");
