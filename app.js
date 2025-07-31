fb54b3e034aa0aca3df04acbc49662f862008440
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-31 13:06:50 +0100

    fix: update server port to 3002 and add message file

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const routes = require('./routes')
 
  const server = http.createServer(routes)
 
-     server.listen(3001);
-     console.log("Server is running on http://localhost:3001");
+     server.listen(3002);
+     console.log("Server is running on http://localhost:3002");
